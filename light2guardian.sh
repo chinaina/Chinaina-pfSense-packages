d98490aa1443ae55884d2bb14a77221ebcf91ba8
Douglas Favaro Ferreira <douglas.ferreira@intranet.local>
2019-03-15 17:23:47 -0300

    Update light2guardian.sh

--- light2guardian.sh
+++ light2guardian.sh
@@ -4,11 +4,13 @@
 cd /usr/local/etc/lightsquid/
 rm group.cfg.sample
 fetch https://raw.githubusercontent.com/chinaina/Chinaina-pfsense-packages/master/group.cfg.sample
+chmod 664 /usr/local/etc/lightsquid/group.cfg.sample
 
 ## Change sample file realname.cfg.sample
 cd /usr/local/etc/lightsquid/
 rm realname.cfg.sample
 fetch https://raw.githubusercontent.com/chinaina/Chinaina-pfsense-packages/master/realname.cfg.sample
+chmod 664 /usr/local/etc/lightsquid/realname.cfg.sample
 
 ## Change config file lightsquid.inc
 cd /usr/local/pkg/
@@ -17,5 +19,3 @@ fetch https://raw.githubusercontent.com/chinaina/Chinaina-pfsense-packages/maste
 ## Change xml file lightsquid.xml
 cd /usr/local/pkg/
 fetch https://raw.githubusercontent.com/chinaina/Chinaina-pfsense-packages/master/pkg-Light2guardian/files/usr/local/pkg/lightsquid.xml
-
-
